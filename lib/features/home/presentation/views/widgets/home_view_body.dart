7095575630d99e815cb9c249ab44896cb4e23827
toalhussein <toalhussein@gmail.com>
2024-09-09 15:04:46 +0300

    Create🔨: creating best seller list view item

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/style.dart';
 import 'package:flutter/material.dart';
 
@@ -16,8 +17,49 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeatuerdBooksListView(),
-          const SizedBox(height: 50,),
-          Text('Best Seller', style: Style.titleMeduim)
+          SizedBox(
+            height: 50,
+          ),
+          Text('Best Seller', style: Style.titleMeduim),
+          BestSellerListViewItem()
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    ),
+                  )),
+            ),
+          ),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('data'),
+              Text('subdata'),
+              Row(
+                children: [Text('19.99 €')],
+              )
+            ],
+          )
         ],
       ),
     );
