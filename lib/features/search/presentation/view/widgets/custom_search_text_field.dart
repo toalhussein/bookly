b634b9e5b828b2d28e0c86a150339f66c9a77b3f
toalhussein <toalhussein@gmail.com>
2024-09-16 12:18:07 +0300

    Create🔨: creating custom search text field

---
+++ lib/features/search/presentation/view/widgets/custom_search_text_field.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomSearchTextField extends StatelessWidget {
+  const CustomSearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        fillColor: Colors.grey.withOpacity(.11111),
+        filled: true,
+        enabledBorder: buildOutlineInputBorder(),
+        focusedBorder: buildOutlineInputBorder(),
+        hintText: 'Search',
+        suffixIcon: Opacity(
+          opacity: .7,
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 19,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder() {
+    return OutlineInputBorder(
+        borderSide: const BorderSide(
+          color: Colors.grey
+        ),
+        borderRadius: BorderRadius.circular(15)
+      );
+  }
+}
