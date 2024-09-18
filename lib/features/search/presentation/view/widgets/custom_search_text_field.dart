d9ab69675f6868290f215edb30ba7fa8e03d796f
toalhussein <toalhussein@gmail.com>
2024-09-18 15:38:12 +0300

    Edit✒️: Change padding

--- lib/features/search/presentation/view/widgets/custom_search_text_field.dart
+++ lib/features/search/presentation/view/widgets/custom_search_text_field.dart
@@ -30,6 +30,6 @@ class CustomSearchTextField extends StatelessWidget {
   OutlineInputBorder buildOutlineInputBorder() {
     return OutlineInputBorder(
         borderSide: const BorderSide(color: Colors.grey),
-        borderRadius: BorderRadius.circular(15));
+        borderRadius: BorderRadius.circular(16));
   }
 }
