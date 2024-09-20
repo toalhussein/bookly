78ff795cfd58543cb20000f1033127ff11f654d1
toalhussein <toalhussein@gmail.com>
2024-09-20 12:06:29 +0300

    Add✒️: implement the featured books method

--- lib/features/home/data/repos/home_repo_impelement.dart
+++ lib/features/home/data/repos/home_repo_impelement.dart
@@ -31,8 +31,23 @@ class HomeRepoImpelement implements HomeRepo {
   }
 
   @override
-  Future<Either<Failuer, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failuer, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:programming');
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
