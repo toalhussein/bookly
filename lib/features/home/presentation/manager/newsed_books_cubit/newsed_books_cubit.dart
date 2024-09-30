0f8074e7af601264ec166d8afcca0ba9735f2e7c
toalhussein <toalhussein@gmail.com>
2024-09-30 21:53:03 +0300

    Done✅: newsed books cubit done

--- lib/features/home/presentation/manager/newsed_books_cubit/newsed_books_cubit.dart
+++ lib/features/home/presentation/manager/newsed_books_cubit/newsed_books_cubit.dart
@@ -2,9 +2,20 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../../data/model/book_model/book_model.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'newsed_books_state.dart';
 
 class NewsedBooksCubit extends Cubit<NewsedBooksState> {
-  NewsedBooksCubit() : super(NewsedBooksInitial());
+  NewsedBooksCubit(this.homeRepo) : super(NewsedBooksInitial());
+   final HomeRepo homeRepo;
+
+  Future<void> fetchNewsedBooks() async {
+    emit(NewsedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(NewsedBooksFailure(failure.errMessage)),
+      (books) => emit(NewsedBooksSucces(books)),
+    );
+  }
 }
